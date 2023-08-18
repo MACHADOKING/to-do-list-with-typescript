b317a0167eb173b9987724a31b43f8e13b550151
Julio Machado <jsmacbusiness@gmail.com>
2023-08-18 18:43:32 -0300

    Feat: Criando header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// CSS
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>Rect + TS Todo</span> @2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+// CSS
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>React + TS Todo</h1>
+    </header>
+  );
+};
+
+export default Header;
