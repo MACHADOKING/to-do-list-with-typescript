13e4c35472efbea7235d437b2c7ba114c56bf5dc
Julio Machado <jsmacbusiness@gmail.com>
2023-08-22 15:48:13 -0300

    Feat: Creating Form

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import styles from "./App.module.css";
 // components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h2>Olá Mundo</h2>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </>
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const TaskList = (props: Props) => {
+  return <div>lista</div>;
+};
+
+export default TaskList;
